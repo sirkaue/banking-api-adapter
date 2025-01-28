46b816d5911cbbdce52dd49e026719a736b405f9
sirkaue <kauekdslopes@gmail.com>
2025-01-28 20:17:08 -0300

    Initial Commit

---
+++ src/main/java/com/sirkaue/bankingapiadapter/BankingApiAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.sirkaue.bankingapiadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApiAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApiAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirkaue/bankingapiadapter/BankApiAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirkaue.bankingapiadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
